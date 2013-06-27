434aa0a87b46de1b2c35d94b7c48d65617743ef3
Neogerot <neogerot@hotmail.com>
2013-06-27 19:39:10 +0530

    js change

--- app.js
+++ app.js
@@ -13,17 +13,25 @@ alert("we're looking for " + filter);
 });
 
 function findContactByName(name, callback) {
-function onError() {
-alert('Error: unable to read contacts');
-};
+
 var fields = ["displayName", "name"],
 options = new ContactFindOptions();
 options.filter = name;
 options.multiple = true;
 // find contacts
-navigator.service.contacts.find(fields, callback, onError,
-options);
+navigator.contacts.find(fields, onSuccess, onError, options);
+navigator.service.contacts.find(fields, callback, onError,options);
 }
+// onSuccess: Get a snapshot of the current contacts
+    //
+    function onSuccess(contacts) {
+        for (var i=0; i<contacts.length; i++) {
+            console.log("Display Name = " + contacts[i].displayName);
+        }
+    }
+	function onError() {
+alert('Error: unable to read contacts');
+};
 if (!filter) {
 // no contents
 return;
