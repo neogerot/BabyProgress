0a8ec041aa2dc915caa0c24a47966f8e7e8421ce
Neogerot <neogerot@hotmail.com>
2013-07-15 13:23:40 +0530

    MSM:Remove unwanted files

--- js/synchronizedata.js
+++ js/synchronizedata.js
@@ -129,7 +129,23 @@ function downloadFile(imagename){
                      fail);
                 */
  
-    }    
+    }  
+    
+     function uploadPhoto(imageURI) {
+            var options = new FileUploadOptions();
+            options.fileKey="file";
+            options.fileName=imageURI.substr(imageURI.lastIndexOf('/')+1);
+            options.mimeType="image/jpeg";
+
+            var params = {};
+            params.value1 = "test";
+            params.value2 = "param";
+
+            options.params = params;
+
+            var ft = new FileTransfer();
+            ft.upload(imageURI, encodeURI("http://some.server.com/upload.php"), win, fail, options);
+        }  
 
 function fail(error) {
     console.log(error.code);
@@ -220,7 +236,7 @@ function LoadMetadata()
 				              		    $eventinfo.append("<div> Last Name: " + this.LastName +"<br></div>");	
 				              		    $eventinfo.append("<div> UniqueID: " + this.UniqueID +"<br></div>");	
 				              		  
-				              		  var imagelocalPath = window.rootFS.fullPath +"/"+ this.Image;
+				              		  var imagelocalPath = window.rootFS.fullPath +"/photos/"+ this.Image;
 				              		  var imageName=this.Image;
 				              		  
 				              		  if(imageName!='')
