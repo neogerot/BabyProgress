874db4a077bf15f560669be283b6fae680afd154
Neogerot <neogerot@hotmail.com>
2013-08-30 11:33:29 +0530

    MSM:200: Language Text English

---
+++ js/languagetext_Hindi.js
@@ -0,0 +1,102 @@
+/* This file will contain the Language text for the Device */
+
+//--------------------------------- LOGIN ---------------------------------------------
+var LOGIN_HEADING_MAIN ="माँ से माँ";
+
+var LOGIN_BUTTON_LOGIN="प्रवेश";
+var LOGIN_BUTTON_BACK="वापस";
+var LOGIN_BUTTON_RESETOPTION="डिवाइस खाली करे";
+var LOGIN_BUTTON_RESET="सावधान सिर्फ डिवाइस खाली करने के लिए दबाये";
+
+var LOGIN_LABEL_USERNAME="उपयोगकर्ता का नाम";
+var LOGIN_TEXTBOX_PLACEHOLDER_USERNAME="उपयोगकर्ता का नाम";
+var LOGIN_TEXTBOX_PLACEHOLDER_PASSWORD="उपयोगकर्ता का पासवर्ड";
+var LOGIN_LABEL_SELECTLOCATION="उपलब्ध स्थान सूची (एक स्थान का चयन करे )";
+
+var LOGIN_MESSAGE_ERROR_AUTHENTICATION="आप इस क्रिया को करने के लिए अधिकृत नहीं हैं";
+var LOGIN_MESSAGE_ERROR_NETWORK="अभी आप इंटरनेट से जुड़े नहीं हैं"; 
+var LOGIN_MESSAGE_BUSY_AUTHENTICATION="उपयोगकर्ता सत्यापन";
+var LOGIN_MESSAGE_BUSY_DOWNLOADING="डेटा प्राप्ति में कार्यरत";
+var LOGIN_MESSAGE_BUSY_DELETEDATA="डिवाइस तैयार की जा रही है";
+var LOGIN_MESSAGE_BUSY_DELETEDATACONFIRM="प्रयोग के लिए डिवाइस तैयार";
+
+
+//-------------------------------------------------------------------------------------
+
+//--------------------------------- INDEX  ---------------------------------------------
+var INDEX_BUTTON_OPTION="विकल्प";
+var INDEX_BUTTON_LOGOUT="बंद करे"; 
+
+var INDEX_LABEL_INFLUENCER="सहायक";
+var INDEX_LABEL_LOCATIONNAME="स्थान का नाम";
+
+
+//-------------------------------------------------------------------------------------
+
+//--------------------------------- GROUPPARTICIPANTS  ---------------------------------------------
+var GROUP_TEXTBOX_PLACEHOLDER_SEARCH="खिलाडी संख्या डाले";
+var GROUP_BUTTON_SEARCH="खोजे";
+var GROUP_BUTTON_BACK="वापस";
+var GROUP_BUTTON_ADD="खिलाडी जोड़े"; 
+var GROUP_LABEL_GROUPLOCATIONNAME="समूह और स्थान का नाम";
+
+//--------------------------------------------------------------------------------------------------
+
+//--------------------------------- GROUPPARTICIPANTS  ---------------------------------------------
+var PARTICIPANT_LABEL_FIRSTNAME="प्रथम नाम";
+var PARTICIPANT_LABEL_LASTNAME="अंतिम नाम";
+
+var PARTICIPANT_BUTTON_BACK="वापस";
+var PARTICIPANT_BUTTON_CAPTUREIMAGE="फोटो ले";
+var PARTICIPANT_BUTTON_ENTRYSUBMIT="सुरक्षित करे"; 
+
+var PARTICIPANT_SELECT_CATEGORY="श्रेणी चुनिये";
+var PARTICIPANT_SELECT_CATEGORY_OPTION_PREGNANT="गर्भवती";
+var PARTICIPANT_SELECT_CATEGORY_OPTION_NEWMOM="नयी माँ";
+var PARTICIPANT_SELECT_CATEGORY_OPTION_PREGNANTANDNEWMOM="गर्भवती और नयी माँ";
+
+var PARTICIPANT_SELECT_INFLUENCER="सहायक चुनिये";
+
+var PARTICIPANT_MESSAGE_BUSY_UPDATERECORD="परिवर्तन सुरक्षित हो रहा है";
+var PARTICIPANT_MESSAGE_BUSY_ADDRECORD="विवरण सुरक्षित हो रहा है";
+
+//--------------------------------------------------------------------------------------------------
+
+
+//--------------------------------- PARTICIPANT DETAILS  ---------------------------------------------
+var PARTICIPANTDETAIL_LABEL_POINTS="अर्जित स्र्पये";
+var PARTICIPANTDETAIL_LABEL_CURRENTOBJECTIVES ="मौजूदा स्तर के लक्ष्य";
+var PARTICIPANTDETAIL_LABEL_NEXTOBJECTIVES ="आपके अगले लक्ष्य है";
+var PARTICIPANTDETAIL_BUTTON_BACK="वापस";
+var PARTICIPANTDETAIL_BUTTON_ENTRYSUBMIT="सुरक्षित करे"; 
+//-----------------------------------------------------------------------------
+
+
+//--------------------------------- LOTTERY  ---------------------------------------------
+var LOTTERY_BUTTON_BACK="वापस";
+var LOTTERY_MESSAGE_WINNER="आज की लाटरी जीती है";
+var LOTTERY_MESSAGE_WINNINGAMOUNT="ईनाम की राशि";
+var LOTTERY_BUTTON_START="आरंभ करे";
+//-----------------------------------------------------------------------------
+
+//--------------------------------- INFLUENCER  ---------------------------------------------
+var INFLUENCER_BUTTON_BACK="वापस";
+var INFLUENCER_BUTTON_ADD="सहायक जोड़े";
+//-------------------------------------------------------------------------------------------
+
+//----------------------------------- SYNCHRONIZE ----------------------------------------------
+var SYNCHRONIZE_BUTTON_BACK="वापस";
+var SYNCHRONIZE_BUTTON_LOTTERY="लाटरी ";
+var SYNCHRONIZE_BUTTON_UPLOADDATA="सर्वर पे डेटा भेजे";
+
+var SYNCHRONIZE_MESSAGE_UPLOADINGDATA="सर्वर पे डेटा जा रहा है";
+var SYNCHRONIZE_MESSAGE_DOWNLOADINGDATA="सर्वर से डेटा आ रहा है";
+var SYNCHRONIZE_MESSAGE_UPLOADINGIMAGES="फोटो सर्वर पे जा रही है";
+var SYNCHRONIZE_MESSAGE_ERRORUPLOADINGDATA="डेटा भेजने में परेशानी";
+var SYNCHRONIZE_MESSAGE_DELETINGLOCALDATA="डिवाइस तैयार की जा रही है";
+var SYNCHRONIZE_MESSAGE_DOWNLOADINGIMAGES="फोटो सर्वर से आ रही है ";
+var SYNCHRONIZE_MESSAGE_UPLOADSUCCESSFUL="कार्य सफलतापूर्वक हो गया";
+var SYNCHRONIZE_MESSAGE_ERROR_NETWORK="अभी आप इंटरनेट से जुड़े नहीं हैं"; 
+
+//-----------------------------------------------------------------------------------------------
+
